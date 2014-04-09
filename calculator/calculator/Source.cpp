b0ca02c1583cf9f6d4b9f1c5db0802048ea30f2a
karan <parikhkaran1991@gmail.com>
2014-04-09 14:29:43 -0400

    Sound Playback

---
+++ calculator/calculator/Source.cpp
@@ -0,0 +1,28 @@
+
+# include<iostream>
+#include <math.h>
+#include <Windows.h>
+#include <MMSystem.h>
+
+using namespace std;
+
+int main(void)
+{
+	/*cout << "This is a simple calculator" << endl;
+	int x, z;
+	cout << "number 1" << endl;
+	cin >> x;
+	cout << "number 2" << endl;
+	cin >> z;
+	cout << "sum     " << x + z << endl;
+	*/
+	PlaySound(TEXT("hype.wav"), NULL, SND_SYNC);
+	PlaySound(TEXT("hype.wav"), NULL, SND_SYNC);
+	PlaySound(TEXT("hype.wav"), NULL, SND_SYNC);
+	PlaySound(TEXT("hype.wav"), NULL, SND_SYNC);
+	PlaySound(TEXT("hype.wav"), NULL, SND_SYNC);
+	system("pause");
+
+	return 0;
+
+}
