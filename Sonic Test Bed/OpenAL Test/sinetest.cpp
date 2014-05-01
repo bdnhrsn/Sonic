4c69d60dd0edd28180bbab66b3609be790068175
Neblix <na494@drexel.edu>
2014-05-01 18:28:24 -0400

    Manually Filled a Buffer with a sine wave

--- Sonic Test Bed/OpenAL Test/sinetest.cpp
+++ Sonic Test Bed/OpenAL Test/sinetest.cpp
@@ -2,6 +2,17 @@
 #include "AL\alc.h"
 #include <stdio.h>
 #include <string>
+#include <math.h>
+
+#ifndef M_PI
+#define M_PI atan(1.0)*4
+#endif
+
+#define CHANNELS 2
+#define SAMPLE_RATE 44100
+#define BITS 16
+#define LENGTH_S 1
+#define T_FRAMES SAMPLE_RATE*LENGTH_S
 
 using namespace std;
 
@@ -12,7 +23,8 @@ static void list_audio_devices(const ALCchar *devices)
 
         fprintf(stdout, "Devices list:\n");
         fprintf(stdout, "----------\n");
-        while (device && *device != '\0' && next && *next != '\0') {
+        while (device && *device != '\0' && next && *next != '\0') 
+		{
                 fprintf(stdout, "%s\n", device);
                 len = strlen(device);
                 device += (len + 1);
@@ -21,18 +33,86 @@ static void list_audio_devices(const ALCchar *devices)
         fprintf(stdout, "----------\n");
 }
 
+void fillSine(ALvoid *bufferData, short channels, unsigned long frames, int sampleRate, float frequency, short amplitude)
+{
+	double time = 0;
+	short *data = (short*)bufferData, val;
+
+	for(unsigned long i = 0; i < frames; i++)
+	{
+		time += 1 /(float)sampleRate;
+		val = (short)(amplitude * sin(2 * M_PI * frequency * time));
+
+		for(short j = 0; j < channels; j++)
+		{
+			*data++ = val;
+		}
+	}
+}
+
 int main()
 {
+	ALCenum error;
 	ALCdevice *device;
 	device = alcOpenDevice(NULL);
-	if(!device);
+	if(!device)
+		printf("Device not found!\n");
+	alGetError();
+	
 
 	ALboolean enumeration;
 	enumeration = alcIsExtensionPresent(NULL, "ALC_ENUMERATION_EXT");
-	if (enumeration == AL_FALSE);
+	if (enumeration == AL_FALSE)
+		printf("no enumeration supported!\n");
 
 	list_audio_devices(alcGetString(NULL, ALC_DEVICE_SPECIFIER));
 
+	ALCcontext *context;
+	context = alcCreateContext(device, NULL);
+	if (!alcMakeContextCurrent(context))
+		printf("Context creation failed!");
+
+	ALfloat listenerOri[] = { 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f };
+	alListener3f(AL_POSITION, 0, 0, 1.0f);
+	alListener3f(AL_VELOCITY, 0, 0, 0);
+	alListenerfv(AL_ORIENTATION, listenerOri);
+
+	ALuint source;
+	alGenSources(1, &source);
+	alSourcef(source, AL_PITCH, 1);
+	alSourcef(source, AL_GAIN, 1);
+	alSource3f(source, AL_POSITION, 0, 0, 0);
+	alSource3f(source, AL_VELOCITY, 0, 0, 0);
+	alSourcei(source, AL_LOOPING, AL_FALSE);
+
+	ALuint buffer;
+	alGenBuffers(1, &buffer);
+
+	ALsizei dataSize = T_FRAMES * CHANNELS * (BITS/8);
+	ALvoid *bufferData = (ALvoid *)malloc(dataSize);
+	fillSine(bufferData, CHANNELS, T_FRAMES, SAMPLE_RATE, 440, 25000);
+	short *data = (short *)bufferData;
+
+
+	unsigned int s = 44100;
+	alBufferData(buffer, AL_FORMAT_STEREO16, bufferData, dataSize, SAMPLE_RATE);
+	alSourcei(source, AL_BUFFER, buffer);
+	alSourcePlay(source);
+
+	ALint source_state;
+	alGetSourcei(source, AL_SOURCE_STATE, &source_state);
+	while (source_state == AL_PLAYING) 
+	{
+        alGetSourcei(source, AL_SOURCE_STATE, &source_state);
+	}
+
+	// cleanup context
+	alDeleteSources(1, &source);
+	alDeleteBuffers(1, &buffer);
+	device = alcGetContextsDevice(context);
+	alcMakeContextCurrent(NULL);
+	alcDestroyContext(context);
 	alcCloseDevice(device);
+
 	return 0;
 }
