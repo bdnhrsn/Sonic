4b5ca4a565324750f2f257dd2884816e5f773ed2
Nabeel Ansari <na494@drexel.edu>
2014-05-02 04:56:15 +0100

    Fixed my branch

--- Sonic Test Bed/OpenAL Test/sinetest.cpp
+++ Sonic Test Bed/OpenAL Test/sinetest.cpp
@@ -12,7 +12,8 @@
 #define SAMPLE_RATE 44100
 #define BITS 16
 #define LENGTH_S 1
-#define T_FRAMES SAMPLE_RATE*LENGTH_S
+#define T_FRAMES 882
+#define NUM_BUFFERS 10
 
 using namespace std;
 
@@ -41,8 +42,6 @@ int main()
 	device = alcOpenDevice(NULL);
 	if(!device)
 		printf("Device not found!\n");
-	alGetError();
-	
 
 	ALboolean enumeration;
 	enumeration = alcIsExtensionPresent(NULL, "ALC_ENUMERATION_EXT");
@@ -65,7 +64,7 @@ int main()
 	alSourcef(source, AL_GAIN, 1);
 	alSource3f(source, AL_POSITION, 0, 0, 0);
 	alSource3f(source, AL_VELOCITY, 0, 0, 0);
-	alSourcei(source, AL_LOOPING, AL_FALSE);
+	alSourcei(source, AL_LOOPING, AL_TRUE);
 
 	ALuint buffer;
 	alGenBuffers(1, &buffer);
@@ -73,20 +72,30 @@ int main()
 	ALsizei dataSize = T_FRAMES * CHANNELS * (BITS/8);
 	ALvoid *bufferData = (ALvoid *)malloc(dataSize);
 	fillSine(bufferData, CHANNELS, T_FRAMES, SAMPLE_RATE, 440, 25000);
-	short *data = (short *)bufferData;
 
-
-	unsigned int s = 44100;
+	//for(int i = 0; i < NUM_BUFFERS; i++)
+	//	alBufferData(buffer[i], AL_FORMAT_STEREO16, bufferData, dataSize, SAMPLE_RATE);	
+	//alSourceQueueBuffers(source, NUM_BUFFERS, buffer);
 	alBufferData(buffer, AL_FORMAT_STEREO16, bufferData, dataSize, SAMPLE_RATE);
-	alSourcei(source, AL_BUFFER, buffer);
+	alSourceQueueBuffers(source, AL_BUFFER, &buffer);
 	alSourcePlay(source);
 
-	ALint source_state;
-	alGetSourcei(source, AL_SOURCE_STATE, &source_state);
-	while (source_state == AL_PLAYING) 
+	while(true);
+	/*short state = 0;
+	while(true)
 	{
-        alGetSourcei(source, AL_SOURCE_STATE, &source_state);
-	}
+		ALint processed;
+		alGetSourcei(source, AL_BUFFERS_PROCESSED, &processed);
+		while(processed--)
+		{
+			alSourceUnqueueBuffers(source, 1, &buffer[state]);
+			alSourceQueueBuffers(source, 1, &buffer[state]);
+
+			state++;
+			if(state == NUM_BUFFERS)
+				state = 0;
+		}
+	}*/
 
 	// cleanup context
 	alDeleteSources(1, &source);
