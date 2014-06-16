1a3a708ec3a724eb0b5a2cd8b7ab9b482a8ed5cb
jasonzust <jing@philadelphiagamelab.org>
2014-06-16 09:23:11 -0400

    Epic failure
    Epic failure

--- include/Mixer3D.h
+++ include/Mixer3D.h
@@ -1,32 +1,36 @@
 #include "complex.h"
 #include "AudioObj.h"
 #include "World.h"
+#include "fft.h"
 
 #ifndef MIXER3D_H
 #define MIXER3D_H
 
 
-
+//Run the Test.cpp for tesing
+//Also, remove the convolutionBox before running the Test.cpp
 class Mixer3D
 {
 public:
 	Mixer3D(int bufSize, int smpRate, int bitD, World* w);
-	void mix();
-	int HRTFLoading(int* pAzimuth, int* pElevation, unsigned int samplerate, unsigned int diffused, complex *&leftFilter, complex *&rightFilter);
+	void mix();//doing everything but now just used for every progress.
+	int HRTFLoading(int* pAzimuth, int* pElevation, unsigned int samplerate, unsigned int diffused, complex *&leftFilter, complex *&rightFilter);//loading filters according to the azimuth and elevation values
 	template <typename T>
-	void write(std::ofstream& stream, const T& t);
+	void write(std::ofstream& stream, const T& t);//writing data but mostly no need to care about this funciton
 	template <typename SampleType>
-	void writeWAVData(const char* outFile, SampleType* buf, size_t bufSize, int sampleRate, short channels);
+	void writeWAVData(const char* outFile, SampleType* buf, size_t bufSize, int sampleRate, short channels);//used to write txt. into wav.
+	void Mixer3D::convolution(complex *input, complex *filter, complex *output, long NSIG, long NFIL, long &NFFT);
 
-	complex *getLeftFilter();
+	complex *getLeftFilter();//retrieving leftFilter for debugging
 private:
+	
 	//There is still one pointer which is being created and deleted everytime when the filter fetch function is called.
-	//It is named 
+	//It is named const short* psTempTaps in the mit_hrtf_lib.c file
 	World *myWorld;
-	complex *input, **output, *result, *clFil, *crFil;
+	complex *input, **output,**outputLeft,**outputRight, *result, *clFil, *crFil;
 	AudioObj **AOList;
 	short *lFil, *rFil, *cbResult;
-	int bufferSize, sampleRate, bitDepth, nTaps;//nTaps represents the size of the filter data
+	long bufferSize, sampleRate, bitDepth, nTaps;//nTaps represents the size of the filter data
 	void reassignWorld(World* w);
 
 };
--- source/Mixer3D.cpp
+++ source/Mixer3D.cpp
@@ -1,5 +1,28 @@
+#include <algorithm>
+#include <math.h>
+#include <string>
+#include <iostream>
+#include <fstream>
+
+
 #include "../include/Mixer3D.h"	
 #include "../include/mit_hrtf_lib.h"
+#include "../include/wav.h"
+
+
+
+
+
+
+using namespace std;
+
+struct wavFileData
+{
+	long n;
+	int sampleRate;
+	int bitDepth;
+	int channels;
+};
 
 Mixer3D::Mixer3D(int bufSize, int smpRate, int bitD, World* w)
 {
@@ -11,9 +34,14 @@ Mixer3D::Mixer3D(int bufSize, int smpRate, int bitD, World* w)
 	input = new complex[bufferSize];
 
 	output = new complex*[World::MAX_OBJ];
+	outputLeft = new complex*[World::MAX_OBJ];
+	outputRight = new complex*[World::MAX_OBJ];
 	for (int i = 0; i < World::MAX_OBJ; i++)
+	{
 		output[i] = new complex[bufferSize * 2];
-
+		outputLeft[i] = new complex[bufferSize];
+		outputRight[i] = new complex[bufferSize];
+	}
 	result = new complex[bufferSize * 2];
 	cbResult = new short[bufferSize * 2];
 	
@@ -84,12 +112,55 @@ void Mixer3D::writeWAVData(const char* outFile, SampleType* buf, size_t bufSize,
 
 void Mixer3D::mix()
 {
+	string inFile = "assets\\input1mono.wav";
+	wavFileData inp;
+	//LOAD THE WAV FILES
+	cout << "Attempting to load wav files..." << endl << endl;
+	input = utility::loadCmpWavData(inFile, &inp.n, &inp.sampleRate, &inp.bitDepth, &inp.channels);
+	cout << "Wav files loading complete!" << endl;
+
 	int Azimuth = 50;
 	int elevation = 0;
 	HRTFLoading(&Azimuth, &elevation, sampleRate, 1, clFil, crFil);
+	convolution(input, clFil, output[0], bufferSize, nTaps, bufferSize);
+
 }
 
 complex *Mixer3D::getLeftFilter()
 {
 	return clFil;
+}
+void Mixer3D::convolution(complex *input, complex *filter,complex *output, long NSIG, long NFIL, long &NFFT)
+{
+	//Check for invalid inputs.
+	if (input == NULL || filter == NULL)
+	{
+		cout << "Could not perform convolution on empty arrays!" << endl;
+		return;
+	}
+
+	bool NFFTChanged = false;
+	//If NFFT not a power of 2, or it is smaller than signal or filter, prompt for new.
+	while (log(NFFT) / log(2) != (int)(log(NFFT) / log(2)) || NFFT < NSIG || NFFT < NFIL)
+	{
+		cout << "Please input a valid NFFT, which is >= NSIG(" << NSIG << ") and >= NFIL(" << NFIL << ") : ";
+		cin >> NFFT;
+		NFFTChanged = true;
+	}
+
+	
+	complex *fInput=input, *fFilter=filter;
+
+	
+	
+	//Perform FFT on both input and filter.
+	CFFT::Forward(fInput, NFFT);
+	CFFT::Forward(fFilter, NFFT);
+
+	for (int i = 0; i < NFFT; i++)
+		output[i] = fInput[i] * fFilter[i];
+
+	CFFT::Inverse(output, NFFT);
+	CFFT::Inverse(fInput, NFFT);
+	CFFT::Inverse(fFilter, NFFT);
 }
